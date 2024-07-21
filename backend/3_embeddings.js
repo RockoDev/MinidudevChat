19ad0251538ea0da2f8311ab5e876ef0e27ed70f
Bernardo Hernández <contacto@rockodev.com>
2024-07-21 02:34:47 -0600

    Update embeddings process
    - Generate embeddings and save them on his own collection.
    - Add an option to generate multiple embeddings in a single call.

--- backend/3_embeddings.js
+++ backend/3_embeddings.js
@@ -1,9 +1,25 @@
+import fs from 'node:fs'
+import minimist from 'minimist'
 import { MongoClient } from 'mongodb'
 import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc.js'
+import { embedMany } from 'ai'
 import { createOllama } from 'ollama-ai-provider'
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
+import { getTranscriptionFilePath } from './config.js'
 
 dayjs.extend(utc)
+
+const argv = minimist(process.argv.slice(2))
+
+let COUNT = (argv.count ?? '1').toString().trim().toLowerCase()
+if ( !(/^\d+$/.test(COUNT)) ) {
+  throw new Error(`Invalid --count value: ${COUNT}. Must be a number.`)
+}
+COUNT = Number(COUNT)
+if ( COUNT <= 0 || COUNT > 1000 ) {
+  throw new Error(`Invalid --count value: ${COUNT}. Must be between 1 and 1000.`)
+}
 
 if ( !process.env.MONGO_CONNECTION_URL ) {
   throw new Error('MONGO_CONNECTION_URL environment variable is required.')
@@ -14,47 +30,98 @@ if ( !process.env.OLLAMA_BASE_URL ) {
 }
 
 const ollama = createOllama({
-  baseURL: process.env.OLLAMA_BASE_URL,
+  baseURL: `${process.env.OLLAMA_BASE_URL}/api`,
 })
 
-const getEmbeddings = async text => {
-  const model = ollama.embedding('llama3')
-  const embeddings = await model.doEmbed(text)
-  return embeddings
-  // const response = await fetch('http://localhost:11434/api/embeddings', {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify({
-  //     model: 'llama3',
-  //     prompt: text,
-  //   }),
-  // })
+const getParagraphs = async file => {
+  const transcription = JSON.parse(await fs.promises.readFile(file, { encoding: 'utf-8' }))
+  const paragraphs = []
+  let paragraph = []
+  transcription.forEach((segment, index) => {
+    const text = segment.text.trim()
+    paragraph.push(text)
+    if ( transcription.length === (index + 1) || ( text.endsWith('.') && !text.endsWith('...') ) ) {
+      paragraphs.push(paragraph.join(' '))
+      paragraph = []
+    }
+  })
+  return paragraphs
+}
+
+const getEmbeddings = async paragraphs => {
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500,
+    chunkOverlap: 0,
+    // separators: ["\n\n", "\n", " ", ""],
+  })
+  const chunks = await splitter.splitText(paragraphs.join('\n'))
+  return embedMany({
+    model: ollama.embedding('nomic-embed-text'),
+    values: chunks,
+  })
 }
 
 const mongodb = await MongoClient.connect(process.env.MONGO_CONNECTION_URL)
-const collection = mongodb.db('midudev').collection('segments')
+const db = mongodb.db('midudev')
 
-const cursor = collection.find({
-  embeddings: { $exists: false },
+const cursor = db.collection('videos').find({
+  transcribed_at: { $exists: true },
+  has_embeddings: { $ne: true },
 }, {
-  limit: 1,
+  projection: {
+    _id: true,
+    id: true,
+    platform: true,
+    title: true,
+    duration: true,
+    url: true,
+  },
+  limit: COUNT,
 })
+
+const saveEmbeddings = async (document, embeddingsResult) => {
+  await db.collection('embeddings').deleteMany({
+    video_id: document.id,
+    video_platform: document.platform,
+  })
+  const { embeddings, values } = embeddingsResult
+  await db.collection('embeddings').insertMany(embeddings.map((embedding, index) => ({
+    created_at: dayjs().utc().toDate(),
+    updated_at: dayjs().utc().toDate(),
+    id: document.id,
+    platform: document.platform,
+    title: document.title,
+    duration: document.duration,
+    url: document.url,
+    text: values[index],
+    embedding,
+  })))
+}
 
 for await ( const document of cursor ) {
   try {
-    console.log(`[+] ${document.text.substring(0, 140)}...`)
+    console.log(`[+] ${document.title.substring(0, 140)}...`)
+    const output = getTranscriptionFilePath(document.platform, document.id)
+    if ( !fs.existsSync(output) ) {
+      console.log('    Transcription file not found.')
+      continue
+    }
+    console.log('    Reading transcription...')
+    const paragraphs = await getParagraphs(output)
     console.log('    Generating embeddings...')
-    const embeddings = await getEmbeddings(document.text)
-    console.log('    Embeddings:', embeddings)
-    // await collection.updateOne({
-    //   _id: document._id,
-    // }, {
-    //   $set: {
-    //     embeddings,
-    //   },
-    // })
+    const embeddings = await getEmbeddings(paragraphs)
+    console.log('    Saving embeddings...')
+    await saveEmbeddings(document, embeddings)
+    await db.collection('videos').updateOne({
+      _id: document._id,
+    }, {
+      $set: {
+        updated_at: dayjs().utc().toDate(),
+        has_embeddings: true,
+      },
+    }, {
+      upsert: false,
+    })
   } catch ( err ) {
     console.error(err)
   }
