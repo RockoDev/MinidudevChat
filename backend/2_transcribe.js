dd1235c842653502d6ea446438599b05366aad64
Bernardo Hernández <contacto@rockodev.com>
2024-07-21 02:31:26 -0600

    Update transcribe process
    - Add option to transcribe multiple files in a single call.
    - Show progress in terminal.

--- backend/2_transcribe.js
+++ backend/2_transcribe.js
@@ -1,6 +1,6 @@
 import fs from 'node:fs'
+import minimist from 'minimist'
 import child_process from 'node:child_process'
-import util from 'node:util'
 import { MongoClient } from 'mongodb'
 import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc.js'
@@ -8,28 +8,64 @@ import { getAudioFilePath, getTranscriptionFilePath } from './config.js'
 
 dayjs.extend(utc)
 
-const exec = util.promisify(child_process.exec)
+const argv = minimist(process.argv.slice(2))
+
+let COUNT = (argv.count ?? '1').toString().trim().toLowerCase()
+if ( !(/^\d+$/.test(COUNT)) ) {
+  throw new Error(`Invalid --count value: ${COUNT}. Must be a number.`)
+}
+COUNT = Number(COUNT)
+if ( COUNT <= 0 || COUNT > 1000 ) {
+  throw new Error(`Invalid --count value: ${COUNT}. Must be between 1 and 1000.`)
+}
 
 if ( !process.env.MONGO_CONNECTION_URL ) {
   throw new Error('MONGO_CONNECTION_URL environment variable is required.')
 }
 
+const spawn = async (...args) => {
+  return new Promise((resolve, reject) => {
+    const process = child_process.spawn(...args)
+    process.on('close', code => {
+      code === 0 ? resolve() : reject(new Error(`Process exited with code ${code}`))
+    })
+  })
+}
+
+const getDurationText = duration => {
+  const hours = Math.floor(duration / 3600)
+  const minutes = Math.floor((duration % 3600) / 60)
+  const seconds = duration % 60
+  if ( hours > 0 ) return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+  return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+}
+
 const mongodb = await MongoClient.connect(process.env.MONGO_CONNECTION_URL)
-const collection = mongodb.db('midudev').collection('videos')
-
-const document = await collection.findOne({
+const db = mongodb.db('midudev')
+const documents = await db.collection('videos').find({
   transcribed_at: { $exists: false },
-})
-
+  platform: 'youtube',
+  duration: { $lt: 3600 },
+}, {
+  projection: {
+    _id: true,
+    id: true,
+    platform: true,
+    title: true,
+    duration: true,
+  },
+  sort: {
+    duration: 1,
+  },
+  limit: COUNT,
+}).toArray()
 await mongodb.close()
 
-;(async () => {
-  if ( !document ) {
-    console.log('No documents to transcribe found.')
-    return
-  }
-  console.log(`[+] ${document.title}`)
+if ( documents.length === 0 ) console.log('No documents to transcribe found.')
 
+const transcribe = async document => {
+  console.log(`[+] ${document.title} (${getDurationText(document.duration)})`)
+  
   const output = getTranscriptionFilePath(document.platform, document.id)
   if ( !fs.existsSync(output) ) {
     const file = getAudioFilePath(document.platform, document.id)
@@ -38,49 +74,39 @@ await mongodb.close()
       return
     }
     console.log('    Transcribing...')
-    await exec(`python transcribe.py ${file} ${output}`)
+    await spawn('python', ['transcribe.py', file, output], { stdio: 'inherit' })
     console.log(`    Transcription: ${output}`)
   }
-
+  
   if ( !fs.existsSync(output) ) {
     console.log('    Transcription file not found.')
     return
   }
-
-  console.log('    Reading transcription...')
-  const transcription = JSON.parse(await fs.promises.readFile(output, { encoding: 'utf-8' }))
-
+  
+  // console.log('    Reading transcription...')
+  // const transcription = JSON.parse(await fs.promises.readFile(output, { encoding: 'utf-8' }))
+  // const paragraphs = []
+  // let paragraph = []
+  // transcription.forEach((segment, index) => {
+  //   const text = segment.text.trim()
+  //   paragraph.push(text)
+  //   if ( transcription.length === (index + 1) || ( text.endsWith('.') && !text.endsWith('...') ) ) {
+  //     paragraphs.push(paragraph.join(' '))
+  //     paragraph = []
+  //   }
+  // })
+  
   console.log('    Updating database...')
   try {
     const mongodb = await MongoClient.connect(process.env.MONGO_CONNECTION_URL)
-    for ( const { id, text, start, end } of transcription ) {
-      const collection = mongodb.db('midudev').collection('segments')
-      await collection.updateOne({
-        video_id: document.id,
-        id,
-      }, {
-        $setOnInsert: {
-          video_id: document.id,
-          created_at: dayjs().utc().toDate(),
-          id,
-        },
-        $set: {
-          updated_at: dayjs().utc().toDate(),
-          text,
-          start,
-          end,
-        },
-      }, {
-        upsert: true,
-      })
-    }
-    const collection = mongodb.db('midudev').collection('videos')
-    await collection.updateOne({
+    const db = mongodb.db('midudev')
+    await db.collection('videos').updateOne({
       _id: document._id,
     }, {
       $set: {
         updated_at: dayjs().utc().toDate(),
         transcribed_at: dayjs().utc().toDate(),
+        // transcription: paragraphs.join('\n'),
       },
     }, {
       upsert: false,
@@ -90,4 +116,8 @@ await mongodb.close()
   } catch ( error ) {
     console.error(error)
   }
-})()
+}
+
+for ( const document of documents ) {
+  await transcribe(document)
+}
--- backend/transcribe.py
+++ backend/transcribe.py
@@ -32,8 +32,37 @@ output_dir = os.path.dirname(ouput_file)
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-result = model.transcribe(input_file, word_timestamps=False, language='es', max_initial_timestamp=None, verbose=True)
+result = model.transcribe(input_file, word_timestamps=False, language='es', max_initial_timestamp=None, verbose=False)
 
 with open(ouput_file, 'w', encoding='utf-8') as file:
     file.write(json.dumps(result['segments'], ensure_ascii=False))
 
+# for segment in result['segments']:
+#     for index, word in enumerate(segment['words']):
+#         print(f'{word["start"]} - {word["end"]} -> {word["word"]}')
+
+# offset_size = 7
+
+# parse_word = lambda word: {
+#     'start': word['start'],
+#     'end': word['end'],
+#     'word': word['word'].strip().lower(),
+#     'probability': word['probability'],
+# }
+
+# parse_words = lambda words: [
+#     parse_word(word) for word in words
+# ]
+
+# get_offsets = lambda words: [
+#     word['word'].strip().lower() for word in words
+# ]
+
+# with open('transcription.txt', 'w', encoding='utf-8') as file:
+#     for segment in result['segments']:
+#         for index in range(len(segment['words'])):
+#             word = parse_word(segment['words'][index])
+#             words_left = len(segment['words']) - (index + 1)
+#             # word['offsets'] = parse_words(segment['words'][index + 1 : index + 1 + offset_size])
+#             word['offsets'] = get_offsets(segment['words'][index + 1 : index + 1 + offset_size])
+#             file.write(json.dumps(word, ensure_ascii=False) + '\n')
